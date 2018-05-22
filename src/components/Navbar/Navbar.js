94fd92426d52bfe3708860c043021cb5e3af2365
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2018-05-22 08:14:40 -0400

    home menu with sidebar pusher

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Menu, Icon } from 'semantic-ui-react'
 
-const Navbar = () => {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
+export class Navbar extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      activeItem: {}
+    }
+  }
+  handleItemClick = (e, { name }) => {
+    this.setState({activeItem: name})
+  }
+  render() {
+    const { handleVisible } = this.props
+    const { activeItem } = this.state
+    return (
+      <Menu>
+        <Menu.Item>
+          <Icon name='bars' onClick={handleVisible}/>
+        </Menu.Item>
+        <Menu.Item
+          name='editorials'
+          active={activeItem === 'editorials'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='reviews'
+          active={activeItem === 'reviews'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='upcomingEvents'
+          active={activeItem === 'upcomingEvents'}
+          onClick={this.handleItemClick}
+        />
+      </Menu>
+    )
+  }
 }
 
 export default Navbar
+
+
+Navbar.propTypes = {
+  handleVisible: PropTypes.func.isRequired
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { Sidebar, Menu, Segment, Icon, Container } from 'semantic-ui-react'
+
 import GuestRoute from './GuestRoute/GuestRoute'
 import AuthRoute from './AuthRoute/AuthRoute'
 import Login from '../containers/Login/Login'
@@ -8,23 +10,50 @@ import Home from '../containers/Home/Home'
 import Dashboard from '../containers/Dashboard/Dashboard'
 import Navbar from '../components/Navbar/Navbar'
 
-
-const routes = ({location, match}) => {
-  const { path } = match
-  return (
-    <div>
-      <Navbar />
-      <Route location={location} exact path='/' component={Home} />
-      <GuestRoute location={location} exact path='/login' component={Login} />
-      <AuthRoute location={location} exact path='/dashboard' component={Dashboard} />
+class Routes extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      visible: false
+    }
+  }
+  handleVisible = () => {
+    this.setState({visible: !this.state.visible})
+  }
+  render(){
+    const { visible } = this.state
+    const { location } = this.props 
+    return(
+      <div style={{height: '100%' }}>
+        <Sidebar.Pushable as={Segment} style={{ minHeight: 800, padding: '1em 0em' }}>
+          <Sidebar as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
+            <Menu.Item name='home'>
+              <Icon name='home' />
+                Home
+            </Menu.Item>
+            <Menu.Item name='login'>
+              <Icon name='camera'/>
+              Login
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Navbar
+              handleVisible={this.handleVisible}
+            />
+            <Route location={location} exact path='/' component={Home} />
+            <GuestRoute location={location} exact path='/login' component={Login} />
+            <AuthRoute location={location} exact path='/dashboard' component={Dashboard} />
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
     </div>
-  )
+    )
+  }
 }
 
-routes.propTypes = {
+Routes.propTypes = {
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired
   }).isRequired
 }
 
-export default routes
+export default Routes
